4701b8f3e8a0f5b69f9dfd26c19068578a82bd16
SiarheiKazakevich <dianrom@tut.by>
2024-01-18 20:58:59 +0300

    feat:  01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,15 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'text.txt');
+
+const readStream = fs.createReadStream(filePath);
+
+readStream.pipe(process.stdout);
+
+readStream.on('error', (err) => {
+  console.error(`Error reading file: ${err.message}`);
+});
+readStream.on('end', () => {
+  console.log('\nReading completed.');
+});
