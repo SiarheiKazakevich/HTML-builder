7e566600982da1c0f21c3b5798f0194aec7eda70
SiarheiKazakevich <dianrom@tut.by>
2024-01-18 22:09:23 +0300

    feat:  02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const filePath = './02-write-file/02-write-file.txt';
+
+const writeStream = fs.createWriteStream(filePath, { flags: 'a' });
+
+console.log('Привет! Введите текст (для завершения введите ".exit")');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+function writeToFile(text) {
+  writeStream.write(`${text}\n`);
+}
+
+rl.on('line', (input) => {
+  if (input === '.exit') {
+    console.log('До свидания!');
+    process.exit();
+  }
+
+  writeToFile(input);
+
+  console.log('Введите еще текст:');
+});
+
+process.on('SIGINT', () => {
+  console.log('До свидания!');
+  process.exit();
+});
