06706b23622c422d716198ed3eb26e9f4afa9e4f
Siarhei Kazakevich <dianrom@tut.by>
2025-01-23 16:07:09 +0300

    feat: add function for collect Html-file

--- 06-build-page/index.js
+++ 06-build-page/index.js
@@ -39,8 +39,24 @@ async function mergeStyles() {
   await fs.writeFile(styleFile, styles.join('\n'), 'utf-8');
 }
 
+//function for collect HTML-file
+async function buildHtml() {
+  let template = await fs.readFile(templateFile, 'utf-8');
+  const componentsTags = template.match(/{{\s*[\w-]+\s*}}/g) || [];
 
+  for (const tag of componentTags) {
+    const componentName = tag.replace(/{{\s*|\s*}}/g, '');
+    const componentPath = path.join(componentsDir, `${componentName}.html`);
 
+    try {
+      const componentContent = await fs.readFile(componentPath, 'utf-8');
+      template = template.replace(tag, componentContent);
+    } catch (error) {
+      console.error(`Не удалось найти компонент: ${componentName}`);
+    }
+  }
+  await fs.writeFile(indexFile, template, 'utf-8');
+}
 
 function processTemplate() {
   try {
