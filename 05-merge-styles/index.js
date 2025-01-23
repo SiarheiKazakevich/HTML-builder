1604a355ed09ccf809f217e1e9958f49fe6a5aa0
Siarhei Kazakevich <dianrom@tut.by>
2025-01-23 18:27:37 +0300

    feat: change sync function on async function

--- 05-merge-styles/index.js
+++ 05-merge-styles/index.js
@@ -1,31 +1,38 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
 const stylesDir = path.join(__dirname, 'styles');
 const distDir = path.join(__dirname, 'project-dist');
 const bundlePath = path.join(distDir, 'bundle.css');
 
-function mergeStyles() {
+async function mergeStyles() {
   try {
-    const files = fs.readdirSync(stylesDir);
+    const files = await fs.readdir(stylesDir);
 
     const cssFiles = files.filter((file) => path.extname(file) === '.css');
 
-    const styles = cssFiles.map((file) => {
-      const filePath = path.join(stylesDir, file);
-      return fs.readFileSync(filePath, 'utf-8');
-    });
+    const styles = await Promise.all(
+      cssFiles.map(async (file) => {
+        const filePath = path.join(stylesDir, file);
+        return fs.readFile(filePath, 'utf-8');
+      }),
+    );
 
-    fs.writeFileSync(bundlePath, styles.join('\n'), 'utf-8');
+    await fs.writeFile(bundlePath, styles.join('\n'), 'utf-8');
 
     console.log('Компиляция стилей завершена.');
   } catch (error) {
     console.error('Произошла ошибка:', error.message);
   }
 }
-
-if (!fs.existsSync(distDir)) {
-  fs.mkdirSync(distDir);
+async function prepareDistDir() {
+  try {
+    await fs.mkdir(distDir, { recursive: true });
+  } catch (error) {
+    console.error('Ошибка при создании папки project-dist:', error.message);
+  }
 }
-
-mergeStyles();
+(async () => {
+  await prepareDistDir();
+  await mergeStyles();
+})();
